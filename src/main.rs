c3231b2832dfc9fcc2d4f21d74f58b361f704bc7
marksweeneyster <marksweeneyster@gmail.com>
2024-03-30 09:42:33 -0600

    starter code

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use std::env;
 use std::error::Error;
 use zeromq::{Socket, SocketRecv};
 
+//extern crate flatbuffers;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let port  = if args.len() > 1 { &args[1] } else { "9876" };
+    let port  = if args.len() > 1 { &args[1] } else { "8883" };
     let topic = if args.len() > 2 { &args[2] } else { "hotdogs" };
 
     let endpoint = "tcp://127.0.0.1:".to_owned() + port;
@@ -19,10 +21,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     socket.subscribe(topic).await?;
 
+    let mut _builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
+
     for i in 0..10 {
         println!("Message {}", i);
         let repl = socket.recv().await?;
-        dbg!(repl);
+        for w in repl.iter() {
+            dbg!(w);
+        }
+        //dbg!(repl);
     }
     Ok(())
 }
