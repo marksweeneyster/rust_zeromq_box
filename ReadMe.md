99c2a54b9cd6f71709d44e6c721e97c6e34a5dee
marksweeneyster <marksweeneyster@gmail.com>
2024-06-01 09:53:58 -0600

    remove hardcoded paths to flatc and protoc

--- ReadMe.md
+++ ReadMe.md
@@ -5,3 +5,8 @@ Currently, the one binary is a localhost SUB client.
 Added _flatbuffers_ messaging.  You must use a new `flatc` compiler, `23.5.26+`. 
 There is a breaking change for rust generated files when mixing old compiler output with new
 _flatbuffers_ libs.
+
+The build step (see **build.rs**) uses the `flatc` and `protoc` compilers. Specify their 
+absolute paths in environment variables:
+ - `FLATC_DIR`
+ - `PROTOC_DIR`
--- build.rs
+++ build.rs
@@ -9,8 +9,9 @@ use protoc_rust;
 fn main() {
     println!("cargo:rerun-if-changed=fb_schema/Monster.fbs");
 
-    // @TODO find a portable solution for "flatc" not on PATH
-    let flatc = Flatc::from_path("D:/tools/vcpkg/installed/x64-windows/tools/flatbuffers/flatc");
+    let flatc_path = std::env::var("FLATC_DIR").unwrap();
+
+    let flatc = Flatc::from_path(flatc_path);
 
     // First check that we have a good `flatc`
     flatc.check().expect("flatc found");
@@ -21,11 +22,12 @@ fn main() {
         ..Default::default()
     }).expect("flatc generated rust file(s)");
 
+    let protoc_path = std::env::var("PROTOC_DIR").unwrap();
     let protos_dir = "target/protos";
     // hmmmm, `protoc_rust` doesn't mkdir (???)
     fs::create_dir_all(protos_dir).expect("Failed to create protos out directory");
     protoc_rust::Codegen::new()
-        .protoc_path("D:/tools/vcpkg/installed/x64-windows/tools/protobuf/protoc")
+        .protoc_path(protoc_path)
         .out_dir(protos_dir)
         .inputs(&["protobuf_schema/address_book.proto"])
         .include("protobuf_schema")
